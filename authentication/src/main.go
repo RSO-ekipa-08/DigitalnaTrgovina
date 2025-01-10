8344aeff2b9a39894c6cea33e047ef6dde6b9792
marindereggi <55254483+marindereggi@users.noreply.github.com>
2025-01-10 21:42:46 +0100

    Add back health check to auth service

--- authentication/src/main.go
+++ authentication/src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net"
+	"net/http"
 
 	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
@@ -20,6 +21,20 @@ func main() {
 		log.Fatalf("Failed to initialize the authenticator: %v", err)
 	}
 
+	// Start HTTP server for health check
+	go func() {
+		http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("OK"))
+		})
+
+		log.Printf("HTTP (/health) server listening on :3000")
+		if err := http.ListenAndServe(":3000", nil); err != nil {
+			log.Fatalf("failed to serve HTTP: %v", err)
+		}
+	}()
+
+	// Start gRPC server
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
